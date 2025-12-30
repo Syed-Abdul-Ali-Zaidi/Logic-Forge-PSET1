95d3faa3f93b3c7b22d3104f1ed0b62e5e8d27ab
Syed Abdul Ali Zaidi <syedabdulali25@gmail.com>
2025-12-31 00:37:26 +0500

    Update Challenge_6__Tower_of_Hanoi_Algorithm.py

--- Challenge_6__Tower_of_Hanoi_Algorithm.py
+++ Challenge_6__Tower_of_Hanoi_Algorithm.py
@@ -1,8 +1,9 @@
-def toh(no_of_dics : int, from_rod, to_rod, aux_rod):
-    if no_of_dics == 1:
+def toh(no_of_disk : int, from_rod, to_rod, aux_rod):
+    if no_of_disk == 1:
         print(f"Move {from_rod} to {to_rod}")
         return
-    toh(no_of_dics - 1, from_rod, aux_rod, to_rod)
+    toh(no_of_disk - 1, from_rod, aux_rod, to_rod)
     toh(1, from_rod, to_rod, aux_rod)
-    toh(no_of_dics - 1, aux_rod, to_rod, from_rod)
+    toh(no_of_disk - 1, aux_rod, to_rod, from_rod)
+
 
